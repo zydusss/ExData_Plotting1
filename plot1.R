b7e13ca9ed36e5be92ab4b1cc2e270cbd3d30473
zydusss <rahulbajaj@hotmail.co.in>
2014-08-11 03:36:28 +0530

    Plot1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+
+# Load Data
+
+dataset<-read.table(file.choose(),sep=";", colClasses=c("character", "character", rep("numeric",7)),na="?",header=TRUE)
+head(dataset)
+
+# convert date and time variables to Date/Time class
+dataset$Time <- strptime(paste(dataset$Date, dataset$Time), "%d/%m/%Y %H:%M:%S")
+dataset$Date <- as.Date(dataset$Date, "%d/%m/%Y")
+# only use data from the dates 2007-02-01 and 2007-02-02
+dates <- as.Date(c("2007-01-02", "2007-02-02"), "%Y-%d-%m")
+dataset <- subset(dataset, Date %in% dates)
+  
+png("plot1.png", width=400, height=400)
+hist(as.numeric(dataset$Global_active_power), main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency", col="red")
+dev.off()
